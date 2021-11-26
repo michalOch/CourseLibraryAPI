be3ddec4e7055073948fbcde7bd3f1f862de5c2c
Michal Och <ochrobot1990@gmail.com>
2021-11-26 10:57:31 +0100

    change return type to ActonResult<IEnumerable<AuthorDto>>

--- CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -19,7 +19,7 @@ namespace CourseLibrary.API.Controllers
             _courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
         }
         [HttpGet()]
-        public IActionResult GetAuthors()
+        public ActionResult<IEnumerable<AuthorDto>> GetAuthors()
         {
             var authorsFromRepo = _courseLibraryRepository.GetAuthors();
             var authors = new List<AuthorDto>();
