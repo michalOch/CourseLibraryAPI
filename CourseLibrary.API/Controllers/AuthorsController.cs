cdf16c8b511d1522deac6e999c4678fb469fa40a
Michal Och <ochrobot1990@gmail.com>
2021-11-26 09:48:19 +0100

    add error handling and implement get author by id

--- CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -1,10 +1,12 @@
 ﻿using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Linq;
 
 namespace CourseLibrary.API.Controllers
 {
     [ApiController]
+    [Route("api/authors")]
     public class AuthorsController : ControllerBase
     {
         private readonly ICourseLibraryRepository _courseLibraryRepository;
@@ -13,11 +15,28 @@ namespace CourseLibrary.API.Controllers
         {
             _courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
         }
-        [HttpGet("api/authors")]
+        [HttpGet()]
         public IActionResult GetAuthors()
         {
             var authorsFromRepo = _courseLibraryRepository.GetAuthors();
-            return new JsonResult(authorsFromRepo);
+            if(authorsFromRepo == null || (authorsFromRepo.Count() == 0))
+            {
+                return NotFound();
+            }
+            return Ok(authorsFromRepo);
+        }
+
+        [HttpGet("{authorId:guid}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+
+            if (authorFromRepo == null)
+            {
+                return NotFound();
+            }   
+            
+            return Ok(authorFromRepo);
         }
     }
 }
