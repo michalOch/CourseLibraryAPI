8ed8173bd8c8407386770453fef8039e937a2649
Michal Och <ochrobot1990@gmail.com>
2021-11-28 13:55:37 +0100

    implement post method for adding course for author

--- CourseLibrary.API/Controllers/CoursesController.cs
+++ CourseLibrary.API/Controllers/CoursesController.cs
@@ -33,7 +33,7 @@ namespace CourseLibrary.API.Controllers
             return Ok(_mapper.Map <IEnumerable<CourseDto>>(coursesForAuthorFromRepo));
         }
 
-        [HttpGet("{courseId}")]
+        [HttpGet("{courseId}", Name = "GetCourseForAuthor")]
         public ActionResult<CourseDto> GetCourseForAuthor(Guid authorId, Guid courseId)
         {
             // Check if the author exists
@@ -49,6 +49,22 @@ namespace CourseLibrary.API.Controllers
             }
 
             return Ok(_mapper.Map<CourseDto>(courseForAuthorFromRepo));
+        }
+
+        [HttpPost]
+        public ActionResult<CourseDto> CreateCourseForAuthor(Guid authorId, CourseForCreationDto course)
+        {
+            if (!_courseLibraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var courseEntity = _mapper.Map<Entities.Course>(course);
+            _courseLibraryRepository.AddCourse(authorId,courseEntity);
+            _courseLibraryRepository.Save();
+
+            var courseToReturn = _mapper.Map<Models.CourseDto>(courseEntity);
+            return CreatedAtRoute("GetCourseForAuthor", new {authorId = authorId, courseId = courseToReturn.Id}, courseToReturn);
         }
     }
 }
