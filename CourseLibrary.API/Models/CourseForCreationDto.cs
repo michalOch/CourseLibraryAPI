70c1f36e2ba8d06b07b91ffb12f1996bcb2cfd36
Michal Och <ochrobot1990@gmail.com>
2021-11-29 09:15:02 +0100

    add input validation on CourseForCreationDto

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -1,8 +1,14 @@
-﻿namespace CourseLibrary.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseLibrary.API.Models
 {
     public class CourseForCreationDto
     {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
+
+        [MaxLength(1500)]
         public string Description { get; set; }
     }
 }
