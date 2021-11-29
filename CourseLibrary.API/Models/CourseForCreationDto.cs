84b4f0cf9896682f0d03871aead1e7a66403187e
Michal Och <ochrobot1990@gmail.com>
2021-11-29 09:24:25 +0100

    add class-level input validation with IValidatableObject

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -1,8 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CourseLibrary.API.Models
 {
-    public class CourseForCreationDto
+    public class CourseForCreationDto : IValidatableObject
     {
         [Required]
         [MaxLength(100)]
@@ -10,5 +11,13 @@ namespace CourseLibrary.API.Models
 
         [MaxLength(1500)]
         public string Description { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(Title == Description)
+            {
+                yield return new ValidationResult("The provided description should be different from the title.", new[] { "CourseForCreationDto" });
+            }
+        }
     }
 }
