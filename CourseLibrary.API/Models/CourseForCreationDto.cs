6fb1c1d24e2e0b51dd4c264ccd16e225fcd29cba
Michal Och <ochrobot1990@gmail.com>
2021-11-29 09:40:27 +0100

    implement custom attribute to validate CourseForCreationDto

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -1,23 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using CourseLibrary.API.ValidationAttributes;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace CourseLibrary.API.Models
 {
-    public class CourseForCreationDto : IValidatableObject
+    [CourseTitleMustBeDifferentFromDescriptionAttribute(ErrorMessage = "Title must be different from description.")]
+    public class CourseForCreationDto /*: IValidatableObject*/
     {
-        [Required]
-        [MaxLength(100)]
+        [Required(ErrorMessage = "You should fill out a title.")]
+        [MaxLength(100, ErrorMessage = "The title should't have move than 100 characters. ")]
         public string Title { get; set; }
 
-        [MaxLength(1500)]
+        [MaxLength(1500, ErrorMessage = "The description shoulnd't have more than 1500 characters. ")]
         public string Description { get; set; }
 
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if(Title == Description)
-            {
-                yield return new ValidationResult("The provided description should be different from the title.", new[] { "CourseForCreationDto" });
-            }
-        }
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if(Title == Description)
+        //    {
+        //        yield return new ValidationResult("The provided description should be different from the title.", new[] { "CourseForCreationDto" });
+        //    }
+        //}
     }
 }
