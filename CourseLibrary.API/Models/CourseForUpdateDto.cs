cb29d8c4dbd8b3763efe8f570ee156c4e9788b2a
Michal Och <ochrobot1990@gmail.com>
2021-11-29 11:14:40 +0100

    add dto for updating course

---
+++ CourseLibrary.API/Models/CourseForUpdateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CourseLibrary.API.Models
+{
+    public class CourseForUpdateDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
